6df6cadb53086cd9fdd15347bbb50726b76edff0
ANUPAM YADAV <79785163+Anupamyadav988@users.noreply.github.com>
2023-03-20 19:39:26 +0530

    Update linearsearch.cpp

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,3 +1,9 @@
+Approach
+
+// We are given an array in which we need to find a given element and return its index if present and -1, if not present.
+// So, we just start from the first index and traverse the whole array up to the (n-1)th element and compare each element with the given number.
+// If at some index they both match, just return the index.
+// If we reach the end of the array and no element from the array matches with the given number, return -1.
 #include<bits/stdc++.h>
 using namespace std;
 int LinearSearch(int array[],int n,int element){
@@ -19,3 +25,6 @@ int main(){
 
     return 0;
 }
+// Time Complexity: O(N) { At the worst case, the whole array would be traversed i.e N elements }.
+
+// Space Complexity: O(1) { There is no extra space being used in this approach }.
